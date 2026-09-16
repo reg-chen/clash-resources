80b6d0d378a877592e2c7b740e84989ab80421a9
Reg Chen <34430957+reg-chen@users.noreply.github.com>
2026-09-17 01:56:19 +0800

    restore YAML-owned proxy group ordering

--- overrides/vpn-providers.js
+++ overrides/vpn-providers.js
@@ -170,17 +170,6 @@ const REGION_DEFS = [
   ['AFRICA', AFRICA, 'fluent-emoji-flat/hut.svg'],
 ];
 
-const VISIBLE_GROUP_ORDER = [
-  'GLOBAL',
-  'PROXY',
-  'AUTO-FAST',
-  'AUTO-SAFE',
-  'LB-HLS',
-  'LB-STICKY',
-  'LB-ROBIN',
-  'LB-CONSISTENT',
-];
-
 function clone(value) {
   return JSON.parse(JSON.stringify(value));
 }
@@ -553,17 +542,11 @@ function main(config) {
   const vendorGroups = [...buildPiaVendorGroups(pia), ...buildSurfsharkVendorGroups(ss)];
   applyRouteGroupPolicy(baseGroups, vendorGroups);
 
-  const baseGroupByName = new Map(baseGroups.map((group) => [group?.name, group]));
-  const primaryGroups = VISIBLE_GROUP_ORDER.map((name) => baseGroupByName.get(name)).filter(Boolean);
-  const primaryNames = new Set(VISIBLE_GROUP_ORDER);
-  const otherBaseGroups = baseGroups.filter((group) => !primaryNames.has(group?.name));
-
   config['proxy-groups'] = [
     ...pia.endpointGroups,
     ...ss.endpointGroups,
-    ...primaryGroups,
+    ...baseGroups,
     ...vendorGroups,
-    ...otherBaseGroups,
   ];
   return config;
 }
